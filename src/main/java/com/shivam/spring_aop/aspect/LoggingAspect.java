6cd476f8092436acbe78c334ac44b4b8e3e652b0
Shivam Aggarwal <shivam01101995@gmail.com>
2020-08-03 22:45:28 +0530

    Configure aspect to run before function execution

---
+++ src/main/java/com/shivam/spring_aop/aspect/LoggingAspect.java
@@ -0,0 +1,15 @@
+package com.shivam.spring_aop.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LoggingAspect {
+	
+	@Before("execution(public String getName())")
+	public void LoggingAdvice() {
+		System.out.println("LoggingAdvice method of aspect logging called");
+	}
+	
+}
+ 

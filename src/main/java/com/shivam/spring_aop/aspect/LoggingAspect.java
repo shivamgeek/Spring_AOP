f841024343a147af2fb2498027fd2b46c07223d2
Shivam Aggarwal <shivam01101995@gmail.com>
2020-08-04 16:24:51 +0530

    Wildcards for advice

--- src/main/java/com/shivam/spring_aop/aspect/LoggingAspect.java
+++ src/main/java/com/shivam/spring_aop/aspect/LoggingAspect.java
@@ -6,9 +6,17 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class LoggingAspect {
 	
-	@Before("execution(public String com.shivam.spring_aop.model.Triangle.getName())")
+	/*
+	 * Use * for matching one or more characters
+	 * Use .. for matching 0 or more characters
+	 * 
+	 * 
+	 */
+	
+	
+	@Before("execution(public * get*(..))")
 	public void LoggingAdvice() {
-		System.out.println("LoggingAdvice method of aspect logging called");
+		System.out.println("LoggingAdvice method of aspect logging invoked");
 	}
 	
 }
